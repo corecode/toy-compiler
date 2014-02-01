83c719bac3dcd5d7686aba54dc6e71b599c9a73c
Simon Schubert <2@0x2c.org>
2014-02-02 00:56:12 +0100

    parse: better space handling

--- parse.rb
+++ parse.rb
@@ -2,23 +2,27 @@ require 'neg'
 
 class SexprParser < Neg::Parser
   parser do
-    expr == list | atom
-    spaces == _("\s\r\n") * 1
-    list == `(` + (expr + (spaces + expr) * 0 ) * 0 + `)`
+    expr == spaces? + (list | atom) + spaces?
+    list == `(` + expr * 0 + `)`
     atom == string | number | symbol
     string == `"` + (_('^"\\\\') + (`\\` + _) * -1) * 0 + `"`
     number == `-` * -1 + _('0-9') * 1
     symbol == (-spaces + _('^()')) * 1
+
+    spaces == _("\s\r\n") * 1
+    spaces? == spaces * -1
   end
 
   translator do
-    on(:expr) {|t| t.results.first}
-    on(:spaces) { throw nil }
-    on(:list) {|t| t.flattened_results}
+    on(:expr) {|t| t.results.first.first}
+    on(:list) {|t| t.results.first}
     on(:atom) {|t| t.results.first}
     on(:string) {|t| t.result[1..-2].gsub(/[\\](.)/, '\1')}
     on(:number) {|t| t.result.to_i}
     on(:symbol) {|t| t.result.to_sym}
+
+    on(:spaces) { throw nil }
+    on(:spaces?) { throw nil }
   end
 end
 
@@ -29,5 +33,8 @@ if __FILE__ == $0
   pp p.parse('-23')
   pp p.parse('"foo"')
   pp p.parse('foo')
+  pp p.parse('((foo))')
+  pp p.parse('()')
+  pp p.parse('(())')
   pp p.parse('(foo bar - 123 "zz\\"to\\\\p")')
 end
