daca8ee9cea617c16dd8f60c7ec8b69850b02bfa
Simon Schubert <2@0x2c.org>
2014-03-08 00:30:17 +0100

    despaghetti macros

--- compile.rb
+++ compile.rb
@@ -12,7 +12,7 @@ class Scope
   end
 
   def [](sym)
-    puts "#{self}: looking up #{sym}"
+    # puts "#{self} in #@fun: looking up #{sym}, have #{@tab.keys.join(" ")}"
     @tab[sym] || @parent[sym]
   end
 
@@ -22,7 +22,7 @@ class Scope
 end
 
 class Function
-  attr_reader :curblk
+  attr_reader :curblk, :mod
 
   def initialize(mod, name, argnames, parentscope)
     @mod = mod
@@ -33,7 +33,7 @@ class Function
     @mod.functions.add(@name, [LLVM::Int]*argnames.count, LLVM::Int) do |f, *argvals|
       @f = f
       @parentscope[name] = {:type => :const, :val => @f}
-      @syms = [Scope.new(self, @parentscope)]
+      @syms = [Scope.new(name, @parentscope)]
 
       self.new_blk!
       argnames.zip(argvals) do |n, v|
@@ -43,12 +43,16 @@ class Function
     end
   end
 
+  def val
+    @f
+  end
+
   def syms
     @syms.last
   end
 
   def push_scope
-    @syms.push Scope.new(self, self.syms)
+    @syms.push Scope.new("XXX push", self.syms)
     if block_given?
       begin
         yield
@@ -92,204 +96,50 @@ class Function
     end
     mem
   end
-end
 
-class Compile
-  module Builtins
-    Handlers = Scope.new(nil, nil)
-
-    {
-      :+ => :add,
-      :- => :sub,
-      :* => :mul,
-      :/ => :sdiv,
-    }.each do |sym, op|
-      h = proc do |fn, args|
-        fn.build do |b|
-          args.reduce do |a1, a2|
-            b.send(op, a1, a2)
-          end
-        end
-      end
-      Handlers[sym] = {:type => :const, :val => h}
-    end
-
-    {
-      '='.to_sym => :eq,
-      :not= => :ne,
-      :< => :slt,
-      :> => :sgt,
-      :<= => :sle,
-      :>= => :sge
-    }.each do |sym, cmp|
-      h = proc do |fn, args|
-        fn.build do |b|
-          args.each_cons(2).map do |a1, a2|
-            b.icmp(cmp, args[0], args[1])
-          end.reduce do |a1, a2|
-            b.and(a1, a2)
-          end
-        end
-      end
-      Handlers[sym] = {:type => :const, :val => h}
-    end
-  end
-
-  def initialize(mod)
-    @m = LLVM::Module.new(mod)
-    @globals = Scope.new(nil, Builtins::Handlers)
-  end
-
-  def defn(name, argnames, body)
-    fn = Function.new(@m, name, argnames, @globals)
-
-    val = gen_body(fn, body)
-    fn.build do |b|
-      b.ret(val)
-    end
-  end
-
-  def cond(fn, args)
+  def gen_body(body)
     val = nil
-
-    outblk = fn.create_blk
-
-    nextblk = fn.curblk
-    phis = args.each_slice(2).map do |c, r|
-      fn.set_blk!(nextblk)
-      nextblk = fn.create_blk
-      resblk = fn.create_blk
-
-      fn.build do |b|
-        cv = gen(fn, c)
-        take = b.icmp(:ne, cv, LLVM::Int(0))
-        b.cond(take, resblk, nextblk)
-      end
-
-      fn.set_blk!(resblk)
-      resval = gen(fn, r)
-      fn.build do |b|
-        b.br outblk
-      end
-      resval
+    body.each do |be|
+      val = self.gen(be)
     end
-
-    fn.set_blk!(nextblk)
-    fn.build do |b|
-      b.br outblk
-    end
-    phis << [nextblk, LLVM::Int(0)]
-
-    fn.set_blk!(outblk)
-    fn.build do |b|
-      val = b.phi(LLVM::Int, Hash[*phis.flatten])
-    end
-
-    fn.set_blk!(outblk)
     val
   end
 
-  def let(fn, assignments, body)
-    fn.push_scope do
-      assignments.each_slice(2) do |sym, expr|
-        if !sym.is_a? Symbol
-          raise RuntimeError, "need symbol in left hand position, got `#{sym}'/#{sym.class} instead"
-        end
-
-        val = gen(fn, expr)
-        fn.create_var(sym, val)
-      end
-
-      val = gen_body(fn, body)
-      val
+  def gen_funcall(fun, args)
+    evald_args = args.map{|e| gen(e)}
+    self.build do |b|
+      b.call(fun, *evald_args)
     end
   end
 
-  def loop_while(fn, test, body)
-    inblk = fn.curblk
-    testblk = fn.create_blk
-    outblk = fn.create_blk
+  def gen_invocation(expr)
+    pred_sym, *args = expr
+    pred = gen(pred_sym)
 
-    fn.build do |b|
-      b.br(testblk)
+    if pred.is_a? LLVM::Value
+      val = gen_funcall(pred, args)
+    elsif pred.respond_to? :call
+      val = pred.call(self, args)
+    else
+      raise RuntimeError, "invalid indentifier `#{pred_sym} (#{pred})'"
     end
-
-    bodyblk = fn.new_blk!
-    bodyval = gen_body(fn, body)
-    fn.build do |b|
-      b.br(testblk)
-    end
-
-    fn.set_blk!(testblk)
-    val = nil
-    fn.build do |b|
-      val = b.phi(LLVM::Int, {inblk => LLVM::Int(0), bodyblk => bodyval})
-      testval = gen(fn, test)
-      cond = b.icmp(:ne, testval, LLVM::Int(0))
-      b.cond(cond, bodyblk, outblk)
-    end
-
-    fn.set_blk!(outblk)
-    val
   end
 
-  def gen(fn, expr)
+  def gen(expr)
     val = nil
     case expr
     when Numeric
       val = LLVM::Int(expr)
     when Array
-      case expr.first
-      when :defn
-        name = expr[1]
-        args = expr[2]
-        body = expr[3..-1]
-        val = defn(name, args, body)
-      when :cond
-        args = expr[1..-1]
-        state, val = cond(state, args)
-      when :set!
-        sym = expr[1]
-        sym = fn.syms[sym]
-        if not sym || sym[:val] != :loc
-          raise RuntimeError, "target `#{args[1]}' is not mutable"
-        end
-        val = gen(fn, expr[2])
-        fn.build do |b|
-          b.store(val, sym[:val])
-        end
-      when :let
-        assignments = expr[1]
-        body = expr[2..-1]
-        val = let(fn, assignments, body)
-      when :while
-        test = expr[1]
-        body = expr[2..-1]
-        val = loop_while(fn, test, body)
-      else
-        final = expr.map{|e| v = gen(fn, e); v}
-        pred = expr.first
-        fun = final.first
-        args = final[1..-1]
-
-        fn.build do |b|
-          if fun.is_a? LLVM::Value
-            val = b.call(fun, *args)
-          elsif fun.respond_to? :call
-            val = fun.call(fn, args)
-          else
-            raise RuntimeError, "invalid indentifier `#{pred}' (#{fun})"
-          end
-        end
-      end
+      val = gen_invocation(expr)
     else
-      val = fn.syms[expr]
+      val = self.syms[expr]
       if val
         case val[:type]
         when :const
           val = val[:val]
         when :loc
-          fn.build do |b|
+          self.build do |b|
             val = b.load(val[:val])
           end
         else
@@ -299,19 +149,184 @@ class Compile
     end
     val
   end
+end
 
-  def gen_body(fn, body)
-    val = nil
-    body.each do |be|
-      val = gen(fn, be)
+class Compile
+  module Builtins
+    Ops_arith = {
+      :+ => :add,
+      :- => :sub,
+      :* => :mul,
+      :/ => :sdiv,
+    }
+
+    Ops_cmp = {
+      '='.to_sym => :eq,
+      :not= => :ne,
+      :< => :slt,
+      :> => :sgt,
+      :<= => :sle,
+      :>= => :sge
+    }
+
+    Handlers = Scope.new("builtins", nil)
+
+    def self.add(sym, &blk)
+      Handlers[sym] = {:type => :const, :val => proc(&blk)}
     end
-    val
+
+    Ops_arith.each do |sym, op|
+      add(sym) do |fn, args|
+        evald_args = args.map{|a| fn.gen(a)}
+        fn.build do |b|
+          evald_args.reduce do |a1, a2|
+            b.send(op, a1, a2)
+          end
+        end
+      end
+    end
+
+    Ops_cmp.each do |sym, cmp|
+      add(sym) do |fn, args|
+        evald_args = args.map{|a| fn.gen(a)}
+        fn.build do |b|
+          evald_args.each_cons(2).map do |a1, a2|
+            b.icmp(cmp, args[0], args[1])
+          end.reduce do |a1, a2|
+            b.and(a1, a2)
+          end
+        end
+      end
+    end
+
+    add(:defn) do |fn, args|
+      name, argnames, *body = args
+
+      fn = Function.new(fn.mod, name, argnames, fn.syms)
+      val = fn.gen_body(body)
+      fn.build do |b|
+        b.ret(val)
+      end
+
+      fn.val
+    end
+
+    add(:cond) do |fn, args|
+      val = nil
+
+      outblk = fn.create_blk
+
+      nextblk = fn.curblk
+      phis = args.each_slice(2).map do |c, r|
+        fn.set_blk!(nextblk)
+        nextblk = fn.create_blk
+        resblk = fn.create_blk
+
+        fn.build do |b|
+          cv = fn.gen(c)
+          take = b.icmp(:ne, cv, LLVM::Int(0))
+          b.cond(take, resblk, nextblk)
+        end
+
+        fn.set_blk!(resblk)
+        resval = fn.gen(r)
+        fn.build do |b|
+          b.br outblk
+        end
+        resval
+      end
+
+      fn.set_blk!(nextblk)
+      fn.build do |b|
+        b.br outblk
+      end
+      phis << [nextblk, LLVM::Int(0)]
+
+      fn.set_blk!(outblk)
+      fn.build do |b|
+        val = b.phi(LLVM::Int, Hash[*phis.flatten])
+      end
+
+      fn.set_blk!(outblk)
+      val
+    end
+
+    add(:let) do |fn, args|
+      assignments, *body = args
+
+      fn.push_scope do
+        assignments.each_slice(2) do |sym, expr|
+          if !sym.is_a? Symbol
+            raise RuntimeError, "need symbol in left hand position, got `#{sym}'/#{sym.class} instead"
+          end
+
+          val = fn.gen(expr)
+          fn.create_var(sym, val)
+        end
+
+        val = fn.gen_body(body)
+        val
+      end
+    end
+
+    add(:while) do |fn, args|
+      test, *body = args
+
+      inblk = fn.curblk
+      testblk = fn.create_blk
+      outblk = fn.create_blk
+
+      fn.build do |b|
+        b.br(testblk)
+      end
+
+      bodyblk = fn.new_blk!
+      bodyval = fn.gen_body(body)
+      fn.build do |b|
+        b.br(testblk)
+      end
+
+      fn.set_blk!(testblk)
+      val = nil
+      fn.build do |b|
+        val = b.phi(LLVM::Int, {inblk => LLVM::Int(0), bodyblk => bodyval})
+        testval = fn.gen(test)
+        cond = b.icmp(:ne, testval, LLVM::Int(0))
+        b.cond(cond, bodyblk, outblk)
+      end
+
+      fn.set_blk!(outblk)
+      val
+    end
+
+    add(:set!) do |fn, args|
+      var_sym, val_sym = args
+
+      var = fn.syms[var_sym]
+      if not var || var[:val] != :loc
+        raise RuntimeError, "target `#{args[1]}' is not mutable"
+      end
+      val = fn.gen(val_sym)
+      fn.build do |b|
+        b.store(val, var[:val])
+      end
+      val
+    end
   end
 
-  def run(mainexpr)
+  def initialize(mod)
+    @m = LLVM::Module.new(mod)
+    @globals = Scope.new("globals", Builtins::Handlers)
+  end
+
+  def run(modexpr)
     LLVM.init_jit
 
-    defn(:main, [], mainexpr)
+    mainfn = Function.new(@m, :main, [], @globals)
+    val = mainfn.gen_body(modexpr)
+    mainfn.build do |b|
+      b.ret val
+    end
 
     $stderr.puts "#### unoptimized:"
     @m.dump
